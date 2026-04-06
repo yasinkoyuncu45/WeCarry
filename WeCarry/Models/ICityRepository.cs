685bf87031c7ffccebf216886d79e841feefd82b
Yasin Koyuncu <yasinkoyuncu4504@gmail.com>
2026-04-06 13:53:20 +0300

    Delete WeCarry directory

--- WeCarry/Models/ICityRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using WeCarry.Models.MVVM;
-
-namespace WeCarry.Models
-{
-    public interface ICityRepository
-    {
-        Task<List<City>> GetAllAsync();
-    }
-}
--- WeCarry/Services/IChatService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WeCarry.Services
-{
-    public interface IChatService
-    {
-        Task SendAsync(int conversationId, int senderUserId, string body);
-        Task<int> MarkReadAsync(int conversationId, int meUserId);
-        Task<bool> UserIsParticipantAsync(int conversationId, int userId);
-    }
-}
